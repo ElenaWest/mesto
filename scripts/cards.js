3e31452736d2df365508b5278a66a5c66f69653c
Elena Vasilenko <elenawest92@yandex.ru>
2023-03-24 23:52:53 +0300

    Исправить замечания 1 ревью ПР2

---
+++ scripts/cards.js
@@ -0,0 +1,27 @@
+//  Карточки "из коробки"
+const initialCards = [
+    {
+      name: 'Москва',
+      link: './images/Moscow.jpg'
+    },
+    {
+      name: 'Владивосток',
+      link: './images/Vladivostok.jpg'
+    },
+    {
+      name: 'Санкт-Петербург',
+      link: './images/SPB.jpg'
+    },
+    {
+      name: 'Волгоград',
+      link: './images/Volgograd.jpg'
+    },
+    {
+      name: 'Казань',
+      link: './images/Kazan.jpg'
+    },
+    {
+      name: 'Рускеала',
+      link: './images/Ruskeala.jpg'
+    }
+  ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,3 @@
-//  Карточки "из коробки"
-const initialCards = [
-    {
-      name: 'Москва',
-      link: './images/Moscow.jpg'
-    },
-    {
-      name: 'Владивосток',
-      link: './images/Vladivostok.jpg'
-    },
-    {
-      name: 'Санкт-Петербург',
-      link: './images/SPB.jpg'
-    },
-    {
-      name: 'Волгоград',
-      link: './images/Volgograd.jpg'
-    },
-    {
-      name: 'Казань',
-      link: './images/Kazan.jpg'
-    },
-    {
-      name: 'Рускеала',
-      link: './images/Ruskeala.jpg'
-    }
-  ];
-
 //  Определение переменных
 const profile = document.querySelector('.profile');
 const editButton = profile.querySelector('.profile__edit-button');
@@ -33,17 +5,17 @@ const addButton = profile.querySelector('.profile__add-button');
 const profileName = profile.querySelector('.profile__name');
 const profileStatus = profile.querySelector('.profile__status');
 const popupProfile = document.querySelector('.popup_type_profile');
-const popupCloseProfile = popupProfile.querySelector('.popup__close');
+const popupCloseProfileButton = popupProfile.querySelector('.popup__close');
 const formProfile = popupProfile.querySelector('.popup__content');
 const popupCards = document.querySelector('.popup_type_picture');
-const popupCloseCards = popupCards.querySelector('.popup__close');
+const popupCloseCardsButton = popupCards.querySelector('.popup__close');
 const formCards = popupCards.querySelector('.popup__content');
 const popupImage = document.querySelector('.popup_type_image');
-const popupCloseImage = popupImage.querySelector('.popup__close');
-const popupName = document.querySelector('#name');
-const popupStatus = document.querySelector('#status');
-const popupPlace = document.querySelector('#place');
-const popupLink = document.querySelector('#link');
+const popupCloseImageButton = popupImage.querySelector('.popup__close');
+const inputNamePopupProfile = document.querySelector('#inputNamePopupProfile');
+const inputStatusPopupProfile = document.querySelector('#inputStatusPopupProfile');
+const inputPlacePopupAddNewCard = document.querySelector('#inputPlacePopupAddNewCard');
+const inputLinkPopupAddNewCard = document.querySelector('#inputLinkPopupAddNewCard');
 const elements = document.querySelector('.elements');
 const elementsList = elements.querySelector('.elements__list');
 const cardsTemplate = document.querySelector('#cards').content.querySelector('.element');
@@ -67,13 +39,12 @@ function handlePreviewImage(popupImageItem) {
   imageFigcaption.textContent = popupImageItem.name;  
 }          
           
-popupCloseImage.addEventListener('click', () => {
+popupCloseImageButton.addEventListener('click', () => {
   popupClose(popupImage);
 });
           
             //  Добавить из коробки, лайк, удалить, увеличить:
-initialCards.forEach(createCards)
-function createCards (item) {
+function createCards(item) {
   const cloneTemplate = cardsTemplate.cloneNode(true);
   const elementTitle = cloneTemplate.querySelector('.element__title');
   const elementPhoto = cloneTemplate.querySelector('.element__photo');
@@ -83,7 +54,6 @@ function createCards (item) {
   elementTitle.textContent = item.name;
   elementPhoto.src = item.link;
   elementPhoto.alt = item.name;
-  elementsList.append(cloneTemplate);
 
   elementLike.addEventListener('click', evt => {
     evt.target.classList.toggle('element__heart_active');
@@ -106,20 +76,25 @@ function createCards (item) {
   return cloneTemplate;
 };
 
+initialCards.forEach((item) => {
+  const card = createCards(item);
+  elementsList.append(card);
+});
+
             //  Открыть/закрыть попап, отправка формы
 addButton.addEventListener('click', () => {
   popupOpen(popupCards);
 });
 
-popupCloseCards.addEventListener('click', () => {
+popupCloseCardsButton.addEventListener('click', () => {
   popupClose(popupCards);
 });
 
 function handleFormCardsSubmit(evt) {
   evt.preventDefault();
   const newCard = createCards ({
-    name: popupPlace.value,
-    link: popupLink.value
+    name: inputPlacePopupAddNewCard.value,
+    link: inputLinkPopupAddNewCard.value
   });
   elementsList.prepend(newCard);
   evt.target.reset();
@@ -128,20 +103,24 @@ function handleFormCardsSubmit(evt) {
 formCards.addEventListener('submit', handleFormCardsSubmit);
 
 //  Профиль: открыть\закрыть попап, отправка формы
-editButton.addEventListener('click', () => {
+function editProfile () {
+  inputNamePopupProfile.value = profileName.textContent;
+  inputStatusPopupProfile.value = profileStatus.textContent;
+}
+
+editButton.addEventListener('click', (editProfile) => {
   popupOpen(popupProfile);
-  popupName.value = profileName.textContent;
-  popupStatus.value = profileStatus.textContent;
+  editProfile();
 });
 
-popupCloseProfile.addEventListener('click', () => {
+popupCloseProfileButton.addEventListener('click', () => {
   popupClose(popupProfile);
 });
 
 function handleFormProfileSubmit(evt) {
+    profileName.textContent = inputNamePopupProfile.value;
+    profileStatus.textContent = inputStatusPopupProfile.value;
     evt.preventDefault();
-    profileName.textContent = popupName.value;
-    profileStatus.textContent = popupStatus.value;
     popupClose (popupProfile);
 }
 formProfile.addEventListener('submit', handleFormProfileSubmit);
